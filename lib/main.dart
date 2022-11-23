9eac65b07265281ff18f566a744f4eff831a52a6
initialds <initialsds@gmail.com>
2022-11-24 00:11:20 +0700

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'ui/screens/home_screen.dart';
+import '/cubit/auth_cubit.dart';
+import '/ui/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: MaterialApp(
+        theme: ThemeData(
+          fontFamily: 'Lato',
+        ),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
 }
